45e1228f2a348cb627a96dee0c035819b1c92d99
nicklaus_ <pedcremo@gmail.com>
2019-01-30 20:55:36 +0100

    ufff2

--- hookScript/unix.sh
+++ hookScript/unix.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+
 `rm -f /tmp/logHook`
+OUTPUT="$(whoami)"
+echo "${OUTPUT}" > /tmp/logHook
 `cd ..`
 OUTPUT="$(git pull)"
-echo "${OUTPUT}" > /tmp/logHook 
+echo "${OUTPUT}" >> /tmp/logHook
 OUTPUT="$(cd && supervisorctl restart all)"
 echo "${OUTPUT}" >> /tmp/logHook
