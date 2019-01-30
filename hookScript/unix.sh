18c1bd0bd1b172b34c28d1d04f57fd4edfbf2115
nicklaus_ <pedcremo@gmail.com>
2019-01-30 19:37:40 +0100

    other web hook test

--- hookScript/unix.sh
+++ hookScript/unix.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 `rm -f /tmp/logHook`
 OUTPUT="$(cd ..)"
-echo OUTPUT > /tmp/logHook
+echo "${OUTPUT}" > /tmp/logHook
 OUTPUT="$(git pull)"
-echo OUTPUT >> /tmp/logHook
+echo "${OUTPUT}" > /tmp/logHook >> /tmp/logHook
 OUTPUT="$(cd)"
-echo OUTPUT >> /tmp/logHook
+echo "${OUTPUT}" > /tmp/logHook >> /tmp/logHook
 OUTPUT="$(supervisorctl restart all)"
-echo OUTPUT >> /tmp/logHook
+echo "${OUTPUT}" > /tmp/logHook >> /tmp/logHook
